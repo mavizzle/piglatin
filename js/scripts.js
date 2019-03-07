c51ed86cc69b8a82bf87a6df19d3c5fd75f59cdf
Student <example@example.com>
2019-03-06 20:06:12 -0800

    create basic business logic to check if a string starts with 'qu' and then changes the word to end with 'quay'

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,47 @@
 // var vowelCheck = function(word) {
-//   var newWord = ''
-//   word.forEach(function(letter){
+//   var lowWord = word.toLowerCase();
+//   var newWord = '';
+//   var consts = '';
+//   newWord.forEach(function(letter){
+//     if (letter != 'a' && letter != 'e' && letter != 'i' && letter != 'o' && letter != 'u') {
+//       consts.pop(letter);
+//     } else {
 //
+//     }
 //   });
 // }
-//
+
 // var pigCheck = function(word) {
 //   for (var index = 0; index < word.length; index +=1) {
 //
 //   }
 // }
 
+var quCheck = function(word) {
+  var lowWord = word.toLowerCase();
+  if (lowWord[0] === 'q' && lowWord[1] === 'u') {
+    return word.slice(2) + word[0] + word[1] + 'ay';
+  } else {
+    return word;
+  }
+
+
+
+
+  // for (var index = 0; index < word.length; index +=1) {
+  //   if (lowWord.charAt(index) != 'a' && lowWord.charAt(index) != 'e' && lowWordindex] != 'i' && lowWord.charAt(index) != 'o' && lowWord.charAt(index) != 'u') {
+  //     return word;
+    // };
+  // };
+};
 
 
 $(document).ready(function(){
-  // $('#resultsdiv').hide();
+  $('#resultsdiv').hide();
 
   $('form#pigform').submit(function(event){
     event.preventDefault();
+    $('#pigphrase').empty();
 
     var pigWord = $('input#piginput').val();
 
@@ -26,5 +50,5 @@ $(document).ready(function(){
   });
 
 
-  
+
 });
